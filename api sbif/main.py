f4231fe802e6a952e77cdbae3078a10fc1625f34
Unknown <a.mario.ulloa@gmail.com>
2019-01-07 00:40:46 -0300

    cambio output
    de mes a fecha completa

--- api sbif/main.py
+++ api sbif/main.py
@@ -14,13 +14,13 @@ meses = []
 subio = {}
 bajo= {}
 mant= {}
-uf=0.0
+uf=0.0 #valor uf inicio mes
 mes =1
 mayor_Uf = 0
 menor_Uf = 0
 mes_Menor= "No hubo meses con baja de UF"
 mes_Mayor= "No hubo meses con alza de UF"
-
+bandera = True
 for x in data['UFs']:
     #print(x['Valor'])
 
@@ -29,19 +29,19 @@ for x in data['UFs']:
     valor = valor.replace(",",".")
     valor = float(valor)
     fecha = (datetime.strptime(x['Fecha'], '%Y-%m-%d'))
+    if bandera:
+        menor_Uf = valor
+        bandera = False
     if valor > mayor_Uf:
         mayor_Uf = valor
-        mes_Mayor = calendar.month_name[fecha.month]
+        mes_Mayor = fecha
     if valor < menor_Uf:
         menor_Uf = valor
-        mes_Menor = calendar.month_name[fecha.month]
-    #print(aux)
-
-    #print(fecha.month)
+        mes_Menor = fecha
     if fecha.month == mes:
         #print(fecha.day)
-        if(fecha.day == 1):
 
+        if(fecha.day == 1):
             uf = valor
         if(fecha.day == calendar.monthrange(2018,fecha.month)[1]):
             mes_abre = calendar.month_name[fecha.month]
@@ -60,6 +60,8 @@ for x in data['UFs']:
 print(data['UFs'])
 subio = sorted(subio.items(), key=lambda kv: kv[1])
 print(subio)
+for x in subio:
+    print(x[0])
 bajo = sorted(bajo.items(), key=lambda kv: kv[1], reverse=True)
 print(bajo)
 mant = sorted(mant.items(), key=lambda kv: kv[0])
